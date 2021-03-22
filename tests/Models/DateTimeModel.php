211e8caaac4b06f97f693b1c9465e1cd5919dd18
hsldymq <hsldymq@qq.com>
2021-03-22 20:46:49 +0800

    update: tests

--- tests/Models/DateTimeModel.php
+++ tests/Models/DateTimeModel.php
@@ -2,7 +2,6 @@
 
 namespace Archman\DataModel\Tests\Models;
 
-use Archman\DataModel\Attributes\Converter;
 use Archman\DataModel\Converters\DateTimeConverter;
 use Archman\DataModel\DataModel;
 use Archman\DataModel\Attributes\Field;
@@ -10,18 +9,18 @@ use Archman\DataModel\Attributes\Field;
 class DateTimeModel extends DataModel
 {
     #[Field('timestamp')]
-    #[Converter(DateTimeConverter::class), DateTimeConverter(DateTimeConverter::TIMESTAMP)]
+    #[DateTimeConverter(DateTimeConverter::TIMESTAMP)]
     public \DateTime $datetime1;
 
     #[Field('timestampDecimal')]
-    #[Converter(DateTimeConverter::class), DateTimeConverter(DateTimeConverter::TIMESTAMP)]
+    #[DateTimeConverter(DateTimeConverter::TIMESTAMP)]
     public \DateTime $datetime2;
 
     #[Field('timestampMS')]
-    #[Converter(DateTimeConverter::class), DateTimeConverter(DateTimeConverter::TIMESTAMP_MS)]
+    #[DateTimeConverter(DateTimeConverter::TIMESTAMP_MS)]
     public \DateTime $datetime3;
 
     #[Field('datetimeStr')]
-    #[Converter(DateTimeConverter::class), DateTimeConverter(DateTimeConverter::ISO_8601)]
+    #[DateTimeConverter(DateTimeConverter::ISO_8601)]
     public \DateTime $datetime4;
 }
