27629d14f322c6e60f4c10526a9572fa8ddf8c73
hsldymq <hsldymq@qq.com>
2021-03-24 12:59:15 +0800

    fix: IntConverter转换时的类型判断错误

--- src/Converters/IntConverter.php
+++ src/Converters/IntConverter.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Archman\DataModel\Converters;
 
 use Archman\DataModel\PropertyType;
+use Archman\DataModel\TypeHelper;
 use Attribute;
 use TypeError;
 
@@ -17,8 +18,17 @@ class IntConverter implements ConverterInterface
 
     public function convert(mixed $fieldValue, PropertyType $type): int
     {
-        if ($this->convertTypes && !$type->isOneOf($this->convertTypes)) {
-            throw new TypeError('int converter: unexpect type');
+        if (is_int($fieldValue)) {
+            return $fieldValue;
+        }
+
+        if ($this->convertTypes) {
+            [$valueType, $isValueObject] = TypeHelper::getValueType($fieldValue);
+            if (!in_array($valueType, $this->convertTypes) &&
+                (!$isValueObject || !in_array('object', $this->convertTypes))
+            ) {
+                throw new TypeError('int converter: unexpect type');
+            }
         }
 
         return intval($fieldValue);
