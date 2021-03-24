13586e5f2ca0d71589988ddf4c6904876dfab2b6
hsldymq <hsldymq@qq.com>
2021-03-24 12:58:30 +0800

    add: IntConverter test

---
+++ tests/PrimitivesConverterTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Archman\DataModel\Tests;
+
+use Archman\DataModel\Tests\Models\DateTimeModel;
+use Archman\DataModel\Tests\Models\IntModel;
+use PHPUnit\Framework\TestCase;
+use TypeError;
+
+class PrimitivesConverterTest extends TestCase
+{
+    public function testIntConverter()
+    {
+        $model = new IntModel([
+            'value1' => false,
+            'value2' => 1.2,
+            'value3' => "123",
+        ]);
+
+        $this->assertEquals(0, $model->value1);
+        $this->assertEquals(1, $model->value2);
+        $this->assertEquals(123, $model->value3);
+    }
+
+    public function testIntConverter_ExpectError()
+    {
+        $this->expectException(TypeError::class);
+
+        new IntModel(['value3' => null]);
+    }
+}
