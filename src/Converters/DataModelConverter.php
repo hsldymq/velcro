445a5112394b5506723f1b8879d563f58e6bfb80
hsldymq <hsldymq@qq.com>
2021-03-24 20:24:57 +0800

    update: DataModelConverter

--- src/Converters/DataModelConverter.php
+++ src/Converters/DataModelConverter.php
@@ -7,13 +7,28 @@ namespace Archman\DataModel\Converters;
 use Archman\DataModel\DataModel;
 use Archman\DataModel\PropertyType;
 use Attribute;
+use TypeError;
 
 #[Attribute]
 class DataModelConverter implements ConverterInterface
 {
+    public function __construct(private string $dataModelSubclass = '')
+    {
+        if ($this->dataModelSubclass && !is_subclass_of($this->dataModelSubclass, DataModel::class)) {
+            throw new TypeError('DataModelConverter: specified class should be a subclass of DataModel');
+        }
+    }
+
     public function convert(mixed $fieldValue, PropertyType $type): DataModel
     {
-        // TODO
-        return $fieldValue;
+        if ($this->dataModelSubclass) {
+            $dataModelClass = $this->dataModelSubclass;
+        } else if ($type->isDataModel()) {
+            $dataModelClass = $type->getDataModelTypes()[0];
+        } else {
+            throw new TypeError('DataModelConverter: the property should be a subclass of DataModel');
+        }
+
+        return new $dataModelClass($fieldValue);
     }
 }
