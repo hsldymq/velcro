456ecd2ef92944d5ecd083d2078aeca74ebf5457
hsldymq <hsldymq@qq.com>
2021-03-24 13:01:21 +0800

    fix: FloatConverter 类型判断错误

--- src/Converters/FloatConverter.php
+++ src/Converters/FloatConverter.php
@@ -5,20 +5,30 @@ declare(strict_types=1);
 namespace Archman\DataModel\Converters;
 
 use Archman\DataModel\PropertyType;
+use Archman\DataModel\TypeHelper;
 use Attribute;
 use TypeError;
 
 #[Attribute]
 class FloatConverter implements ConverterInterface
 {
-    public function __construct(private array $convertTypes = [])
+    public function __construct(private array $expectTypes = [])
     {
     }
 
     public function convert(mixed $fieldValue, PropertyType $type): float
     {
-        if ($this->convertTypes && !$type->isOneOf($this->convertTypes)) {
-            throw new TypeError('float converter: unexpect type');
+        if (is_float($fieldValue)) {
+            return $fieldValue;
+        }
+
+        if ($this->expectTypes) {
+            [$valueType, $isValueObject] = TypeHelper::getValueType($fieldValue);
+            if (!in_array($valueType, $this->expectTypes) &&
+                (!$isValueObject || !in_array('object', $this->expectTypes))
+            ) {
+                throw new TypeError('float converter: unexpect type');
+            }
         }
 
         return floatval($fieldValue);
